f38cde4ef6777825a1694eaf53dbdc4811870bee
trancefreak77 <christian.valentin@tele2.at>
2014-08-19 18:18:06 +0200

    Updated...

--- LampShowCreator/Form1.Designer.cs
+++ LampShowCreator/Form1.Designer.cs
@@ -24,9 +24,11 @@
     /// </summary>
     private void InitializeComponent() {
       this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-      this.SaveAsBtn = new System.Windows.Forms.Button();
-      this.LoadBtn = new System.Windows.Forms.Button();
       this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+      this.SaveLampShowBtn = new System.Windows.Forms.Button();
+      this.DefineLampNamesBtn = new System.Windows.Forms.Button();
+      this.LoadBtn = new System.Windows.Forms.Button();
+      this.CreateStatesBtn = new System.Windows.Forms.Button();
       ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
       this.splitContainer1.Panel1.SuspendLayout();
       this.splitContainer1.Panel2.SuspendLayout();
@@ -43,35 +45,16 @@
       // splitContainer1.Panel1
       // 
       this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
-      this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
       // 
       // splitContainer1.Panel2
       // 
+      this.splitContainer1.Panel2.Controls.Add(this.SaveLampShowBtn);
+      this.splitContainer1.Panel2.Controls.Add(this.DefineLampNamesBtn);
       this.splitContainer1.Panel2.Controls.Add(this.LoadBtn);
-      this.splitContainer1.Panel2.Controls.Add(this.SaveAsBtn);
+      this.splitContainer1.Panel2.Controls.Add(this.CreateStatesBtn);
       this.splitContainer1.Size = new System.Drawing.Size(568, 565);
       this.splitContainer1.SplitterDistance = 520;
       this.splitContainer1.TabIndex = 1;
-      // 
-      // SaveAsBtn
-      // 
-      this.SaveAsBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this.SaveAsBtn.Location = new System.Drawing.Point(481, 6);
-      this.SaveAsBtn.Name = "SaveAsBtn";
-      this.SaveAsBtn.Size = new System.Drawing.Size(75, 23);
-      this.SaveAsBtn.TabIndex = 0;
-      this.SaveAsBtn.Text = "Save as...";
-      this.SaveAsBtn.UseVisualStyleBackColor = true;
-      // 
-      // LoadBtn
-      // 
-      this.LoadBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this.LoadBtn.Location = new System.Drawing.Point(400, 6);
-      this.LoadBtn.Name = "LoadBtn";
-      this.LoadBtn.Size = new System.Drawing.Size(75, 23);
-      this.LoadBtn.TabIndex = 1;
-      this.LoadBtn.Text = "Load from file...";
-      this.LoadBtn.UseVisualStyleBackColor = true;
       // 
       // richTextBox1
       // 
@@ -85,6 +68,47 @@
       this.richTextBox1.Text = "";
       this.richTextBox1.WordWrap = false;
       // 
+      // SaveLampShowBtn
+      // 
+      this.SaveLampShowBtn.Dock = System.Windows.Forms.DockStyle.Right;
+      this.SaveLampShowBtn.Location = new System.Drawing.Point(233, 0);
+      this.SaveLampShowBtn.Name = "SaveLampShowBtn";
+      this.SaveLampShowBtn.Size = new System.Drawing.Size(106, 41);
+      this.SaveLampShowBtn.TabIndex = 3;
+      this.SaveLampShowBtn.Text = "Save Lamp Show";
+      this.SaveLampShowBtn.UseVisualStyleBackColor = true;
+      // 
+      // DefineLampNamesBtn
+      // 
+      this.DefineLampNamesBtn.Dock = System.Windows.Forms.DockStyle.Left;
+      this.DefineLampNamesBtn.Location = new System.Drawing.Point(0, 0);
+      this.DefineLampNamesBtn.Name = "DefineLampNamesBtn";
+      this.DefineLampNamesBtn.Size = new System.Drawing.Size(128, 41);
+      this.DefineLampNamesBtn.TabIndex = 2;
+      this.DefineLampNamesBtn.Text = "Define Lamp Names";
+      this.DefineLampNamesBtn.UseVisualStyleBackColor = true;
+      // 
+      // LoadBtn
+      // 
+      this.LoadBtn.Dock = System.Windows.Forms.DockStyle.Right;
+      this.LoadBtn.Location = new System.Drawing.Point(339, 0);
+      this.LoadBtn.Name = "LoadBtn";
+      this.LoadBtn.Size = new System.Drawing.Size(108, 41);
+      this.LoadBtn.TabIndex = 1;
+      this.LoadBtn.Text = "Load Lamp Show";
+      this.LoadBtn.UseVisualStyleBackColor = true;
+      // 
+      // CreateStatesBtn
+      // 
+      this.CreateStatesBtn.Dock = System.Windows.Forms.DockStyle.Right;
+      this.CreateStatesBtn.Location = new System.Drawing.Point(447, 0);
+      this.CreateStatesBtn.Name = "CreateStatesBtn";
+      this.CreateStatesBtn.Size = new System.Drawing.Size(121, 41);
+      this.CreateStatesBtn.TabIndex = 0;
+      this.CreateStatesBtn.Text = "Create Lamp States";
+      this.CreateStatesBtn.UseVisualStyleBackColor = true;
+      this.CreateStatesBtn.Click += new System.EventHandler(this.CreateStatesBtn_Click);
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -92,7 +116,7 @@
       this.ClientSize = new System.Drawing.Size(568, 565);
       this.Controls.Add(this.splitContainer1);
       this.Name = "Form1";
-      this.Text = "Form1";
+      this.Text = "LampShowCreator";
       this.splitContainer1.Panel1.ResumeLayout(false);
       this.splitContainer1.Panel2.ResumeLayout(false);
       ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
@@ -105,8 +129,10 @@
 
     private System.Windows.Forms.SplitContainer splitContainer1;
     private System.Windows.Forms.Button LoadBtn;
-    private System.Windows.Forms.Button SaveAsBtn;
+    private System.Windows.Forms.Button CreateStatesBtn;
     private System.Windows.Forms.RichTextBox richTextBox1;
+    private System.Windows.Forms.Button DefineLampNamesBtn;
+    private System.Windows.Forms.Button SaveLampShowBtn;
   }
 }
 
--- LampShowCreator/Form1.cs
+++ LampShowCreator/Form1.cs
@@ -13,8 +13,77 @@ namespace LampShowCreator {
       InitializeComponent();
     }
 
-    private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e) {
+    private void CreateStatesBtn_Click(object sender, EventArgs e) {
+      // Split the text by \n
+      string[] textLines = richTextBox1.Text.Split('\n');
+      Dictionary<string, List<byte>> lampTimeDictionary = new Dictionary<string, List<byte>>();
 
+      // Get the identifier (Lamp or Coil).
+      foreach (var textLine in textLines) {
+        bool isLamp = true;
+        int colonIndex = textLine.IndexOf(':');
+        if (colonIndex >= 0) {
+          // We found an identifier.
+          if (textLine.Substring(0, colonIndex).ToLower() == "coil") {
+            // It's a coil.
+            isLamp = false;
+          }
+        } else {
+          colonIndex = 0;
+        }
+
+        // Get name of lamp.
+        int pipeIndex = textLine.IndexOf('|');
+        if (pipeIndex < 0) {
+          // Not found, ignore this line.
+          continue;
+        }
+
+        string lampName = textLine.Substring(colonIndex + 1, (pipeIndex - colonIndex - 1)).Replace("\t", "");
+        if (!lampTimeDictionary.ContainsKey(lampName)) {
+          lampTimeDictionary.Add(lampName, new List<byte>());
+        }
+
+        // Now go through timeline...
+        for (int i = 0; i < (textLine.Length - pipeIndex - 2); i++) {
+          if (textLine[pipeIndex + 2 + i] == '.') {
+            lampTimeDictionary[lampName].Add(1);
+          } else {
+            lampTimeDictionary[lampName].Add(0);
+          }
+        }
+      }
+
+      // Now we have parsed the lamp show text lines.
+      // Loop through each 1/32 time point and find out the
+      // differences between the previous one. Only create
+      // states for different lamp states.
+      // Initialize lamp states.
+      Dictionary<string, byte> currentLampStates = new Dictionary<string, byte>();
+      foreach (var lampKey in lampTimeDictionary.Keys) {
+        if (!currentLampStates.ContainsKey(lampKey)) {
+          currentLampStates.Add(lampKey, 255);  // 255 means not initialized.
+        }
+      }
+
+      int currentState = 1;
+      string stateMachineString = "switch (_currentState) {\n";
+
+      for (int column = 0; column < 100; column ++) {
+        stateMachineString += "  case " + currentState + ":\n";
+        foreach (var lampKey in lampTimeDictionary.Keys) {
+          if (lampTimeDictionary[lampKey][column] != currentLampStates[lampKey]) {
+            currentLampStates[lampKey] = lampTimeDictionary[lampKey][column];
+            stateMachineString = string.Concat(stateMachineString, "    lamps[" + lampKey + "] = " + currentLampStates[lampKey] + ";\n");
+          }
+        }
+
+        stateMachineString += "    _currentState++;\n    break;\n";
+        currentState++;
+      }
+
+      MessageBox.Show(stateMachineString);
     }
+
   }
 }
